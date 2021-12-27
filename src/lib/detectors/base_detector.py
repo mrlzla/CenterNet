9e121057954c4672c1b756c0951269e79cb8c801
Vlad Artemyev <mrlzlamogilev@gmail.com>
2021-12-27 18:30:06 +0300

    Fix

--- src/lib/detectors/base_detector.py
+++ src/lib/detectors/base_detector.py
@@ -139,6 +139,6 @@ class BaseDetector(object):
     if self.opt.debug >= 1:
       self.show_results(debugger, image, results)
     
-    return {'results': results, 'tot': tot_time, 'load': load_time,
+    return {'results': results, 'multi_pose': debugger.imgs['multi_pose'],'tot': tot_time, 'load': load_time,
             'pre': pre_time, 'net': net_time, 'dec': dec_time,
             'post': post_time, 'merge': merge_time}
